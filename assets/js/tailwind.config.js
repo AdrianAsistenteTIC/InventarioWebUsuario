b3714b5b97987871ddcb8b6f28540877f11c1ea9
LINO ADRIAN PAEZ REYES <asistente.tecnologia@pacolombia.com.co>
2024-07-25 15:06:28 -0500

    Formulario Web - Usuario
    Proyecto Web.

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,83 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  // add the folders and files from your templates
+  content: [
+    "./*.html",
+    "./layouts/**/*.html",
+    "./components/**/*.html",
+    "./assets/**/*.{js,ts,jsx,tsx}"
+  ],
+
+  // make sure to safelist these classes when using purge
+  safelist: [
+    'w-64',
+    'w-1/2',
+    'rounded-l-lg',
+    'rounded-r-lg',
+    'bg-gray-200',
+    'grid-cols-4',
+    'grid-cols-7',
+    'h-6',
+    'leading-6',
+    'h-9',
+    'leading-9',
+    'shadow-lg'
+  ],
+
+  // enable dark mode via class strategy
+  darkMode: 'class',
+
+  theme: {
+    extend: {
+      colors: {
+        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+      }
+    },
+    fontFamily: {
+      'body': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      'sans': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ]
+    }
+  },
+  build: {
+    sourcemap: true,
+    sourcemapFile: "./",
+    emptyOutDir: true,
+  },
+  plugins: [
+    // include Flowbite as a plugin in your Tailwind CSS project
+    require('flowbite/plugin')
+  ]
+}
